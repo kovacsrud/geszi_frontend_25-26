c9537d75ed1287b454c71bed90eadea86c289b9c
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2025-12-02 18:09:29 +0100

    tanulók listája

--- tanulok_app/frontend/src/components/Tanulo.jsx
+++ tanulok_app/frontend/src/components/Tanulo.jsx
@@ -1,9 +1,20 @@
-
-
-function Tanulo() {
+function Tanulo({tanulo}) {
   return (
-    <div>Tanulo</div>
-  )
+    <div className="m-5 card card-dash bg-emerald-100 w-96">
+      <div className="card-body">
+        <h2 className="card-title">{tanulo.vezeteknev}{tanulo.keresztnev}</h2>
+        <p>
+          Magasság:{tanulo.magassag}
+        </p>
+        <p>
+          Email:{tanulo.email}
+        </p>
+        <div className="card-actions justify-end">
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Tanulo
+export default Tanulo;
--- tanulok_app/frontend/src/components/Tanulok.jsx
+++ tanulok_app/frontend/src/components/Tanulok.jsx
@@ -1,8 +1,30 @@
+import Tanulo from "./Tanulo"
+import { useState,useEffect } from "react";
 
 
 function Tanulok() {
+  const [tanulok,setTanulok]=useState([]);
+
+  const getTanulok=()=>{
+    fetch(`${import.meta.env.VITE_BASE_URL}/tanulok`)
+    .then(res=>res.json())
+    .then(adat=>setTanulok(adat))
+    .catch(err=>alert(err));
+  }
+
+  useEffect(()=>{
+    getTanulok();
+  },[])
+
   return (
-    <div>Tanulok</div>
+    <div>
+      <h1 className="text-3xl font-bold text-center text-emerald-800">Tanulóink:</h1>
+      <div className="flex flex-row flex-wrap items-center justify-center">
+        {
+          tanulok.map((tanulo)=>(<Tanulo key={tanulo.id} tanulo={tanulo} />))
+        }
+      </div>
+    </div>
   )
 }
 
