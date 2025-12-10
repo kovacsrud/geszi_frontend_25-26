aa51e9f98928e0f5263d8430bac119b9c37aa05e
kovacsrud <kovacs.rud@gmail.com>
2025-12-10 13:39:31 +0100

    Induló kutya projekt

---
+++ react_kutyak/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+   
+      <div>
+        <h1 className="text-3xl font-bold text-center text-sky-500">Tailwind CSS</h1>
+        
+      </div>
+  )
+}
+
+export default App
