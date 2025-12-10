1470895005cdaf84f7f73ac8d9446e1e5d06421e
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2025-12-10 17:41:38 +0100

    Menü kialakítása

--- react_kutyak/frontend/src/components/Menu.jsx
+++ react_kutyak/frontend/src/components/Menu.jsx
@@ -1,9 +1,29 @@
-
+import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <div className="navbar bg-lime-300 shadow-sm">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">Main</Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/kutyanevek">Kutyanevek</Link>
+          </li>
+           <li>
+            <Link to="/kutyafajtak">Kutyafajtak</Link>
+          </li>
+           <li>
+            <Link to="/rendelesiadatok">Rendelési adatok</Link>
+          </li>
+           <li>
+            <Link to="/kutyanevform">Új kutyanév</Link>
+          </li>         
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
