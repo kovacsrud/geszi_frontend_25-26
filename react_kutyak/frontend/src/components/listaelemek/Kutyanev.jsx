8c14994e8fdcea7562cdd9779095f4a8c023958b
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2025-12-10 17:45:32 +0100

    Kutyanév card kialakítása

--- react_kutyak/frontend/src/components/listaelemek/Kutyanev.jsx
+++ react_kutyak/frontend/src/components/listaelemek/Kutyanev.jsx
@@ -1,9 +1,17 @@
-
-
-function Kutyanev() {
+function Kutyanev({ kutyanev }) {
   return (
-    <div>Kutyanev</div>
-  )
+    <div className="m-5 card card-border bg-lime-400 w-96">
+      <div className="card-body">
+        <h2 className="card-title">{kutyanev.kutyanev}</h2>
+        <p>
+         Id:{kutyanev.Id}
+        </p>
+        <div className="card-actions justify-end">
+         
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Kutyanev
+export default Kutyanev;
