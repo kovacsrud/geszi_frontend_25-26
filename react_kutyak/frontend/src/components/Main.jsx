79383aa1451a074106d22704ed3d313eebb2691f
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2025-12-10 17:36:07 +0100

    Main komponens kialakítása

--- react_kutyak/frontend/src/components/Main.jsx
+++ react_kutyak/frontend/src/components/Main.jsx
@@ -1,9 +1,17 @@
-
-
 function Main() {
   return (
-    <div>Main</div>
-  )
+    <div className="hero bg-lime-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Állatorvosi rendelő</h1>
+          <p className="py-6">
+            Kutyája a legjobb kezekben van!
+          </p>
+          <button className="btn btn-primary">Tovább</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
