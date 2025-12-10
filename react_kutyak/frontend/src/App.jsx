b922c8aa6289e737a3a99330b265da3f56b0fe3f
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2025-12-10 17:31:04 +0100

    navigáció kialakítása

--- react_kutyak/frontend/src/App.jsx
+++ react_kutyak/frontend/src/App.jsx
@@ -1,3 +1,12 @@
+import { KutyaProvider } from "./context/KutyaContext";
+import Kutyanevek from "./components/Kutyanevek";
+import Kutyafajtak from "./components/Kutyafajtak";
+import RendelesiAdatok from "./components/RendelesiAdatok";
+import KutyanevForm from "./components/formok/KutyanevForm";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom';
+
 
 
 function App() {
@@ -6,7 +15,19 @@ function App() {
   return (
    
       <div>
-        <h1 className="text-3xl font-bold text-center text-sky-500">Tailwind CSS</h1>
+        <KutyaProvider>
+          <BrowserRouter>
+            <Menu />
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/kutyanevek" element={<Kutyanevek />} />
+              <Route path="/kutyafajtak" element={<Kutyafajtak />} />
+              <Route path="/rendelesiadatok" element={<RendelesiAdatok />} />
+              <Route path="/kutyanevform" element={<KutyanevForm />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </BrowserRouter>
+        </KutyaProvider>
         
       </div>
   )
