22be1240233c858d1e8c7f161e8d78f9c72f6dce
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2025-12-10 18:59:19 +0100

    módosítás és törlés

--- react_kutyak/frontend/src/components/listaelemek/Kutyanev.jsx
+++ react_kutyak/frontend/src/components/listaelemek/Kutyanev.jsx
@@ -1,4 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import KutyaContext from "../../context/KutyaContext";
+
 function Kutyanev({ kutyanev }) {
+  const navigate=useNavigate();
+  const {backendMuvelet}=useContext(KutyaContext);
+
+  const modosit=()=>{
+    navigate('/kutyanevform',{state:{kutyanev}})    
+  }
+  const torol=()=>{
+    const url="http://localhost:8000/kutyanevek";
+    backendMuvelet(kutyanev,'DELETE',url);
+  }
+
+
   return (
     <div className="m-5 card card-border bg-lime-400 w-96">
       <div className="card-body">
@@ -7,8 +23,8 @@ function Kutyanev({ kutyanev }) {
          Id:{kutyanev.Id}
         </p>
         <div className="card-actions justify-end">
-         <button className="btn btn-primary">Módosítás</button>
-         <button className="btn btn-primary">Törlés</button>
+         <button onClick={()=>modosit()} className="btn btn-primary">Módosítás</button>
+         <button onClick={()=>torol()} className="btn btn-primary">Törlés</button>
         </div>
       </div>
     </div>
