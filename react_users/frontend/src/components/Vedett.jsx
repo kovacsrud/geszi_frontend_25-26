9efdbd958a11b0e8ab49711880209c45317f474e
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2026-01-23 17:37:29 +0100

    Védett végpont tesztelése

--- react_users/frontend/src/components/Vedett.jsx
+++ react_users/frontend/src/components/Vedett.jsx
@@ -1,8 +1,41 @@
-
+import { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Vedett() {
+  const navigate=useNavigate();
+  const[vedettInfo,setVedettInfo]=useState(null);
+  const token=sessionStorage.getItem('usertoken');
+
+  const getVedettAdat=()=>{
+    fetch("http://localhost:8000/api/user/vedett",{
+      method:"GET",
+      headers:{
+        "Content-type":"application/json",
+        "Authorization":`Bearer ${token}`
+        
+      }
+    })
+    .then(res=>res.text())
+    .then(adat=>{
+      if(!adat.message){
+        setVedettInfo(adat);
+      } else {
+        alert(adat.message);
+        navigate('/login');
+      }
+    })
+    .catch(err=>alert(err));
+  }
+
+  useEffect(()=>{
+    getVedettAdat();
+  },[])
+
   return (
-    <div>Vedett</div>
+    <div>
+      <h1 className="text-3xl text-center font-bold text-sky-800 m-5">Védett info</h1>
+      <h2 className="text-2xl text-center">{vedettInfo}</h2>
+    </div>
   )
 }
 
