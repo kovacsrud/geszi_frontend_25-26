c1ed9f42c92d20eedfabb41b70d330ab9365d1b7
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2026-02-16 17:47:48 +0100

    Listaelem komponens

--- react_users_files/frontend/src/components/Image.jsx
+++ react_users_files/frontend/src/components/Image.jsx
@@ -1,9 +1,29 @@
+import { useContext } from "react";
+import ImageContext from "../context/ImageContext";
 
+function Image({path,image}) {
+  const {imageName,_id}=image;
+  const {imgDelete}=useContext(ImageContext);
 
-function Image() {
   return (
-    <div>Image</div>
-  )
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img
+          src={`http://localhost:8000/${path}/${imageName}`}
+          alt=""
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{imageName}</h2>
+        <p>
+          
+        </p>
+        <div className="card-actions justify-end">
+          <button onClick={()=>imgDelete(_id)} className="btn btn-primary">Törlés</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Image
+export default Image;
